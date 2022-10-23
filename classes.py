c66ead663bb3b44708939b7e934e7dbda9a66083
Djan Gural Tanova <guralcan@gmail.com>
2022-10-23 13:02:40 +0100

    added some class definition

--- classes.py
+++ classes.py
@@ -1,1 +1,26 @@
 #Will contain the parent class for sensors
+
+
+class Sensor:
+    def __init__(CANID, Name, Bytes, Dist, Inst):
+        self.CANID = CANID
+        self.name = Name
+        self.bytes = Bytes
+        self.dist = Dist
+        self.inst = Inst
+        self.data = []
+
+    def distData(self):
+        distTotal = 0
+        distList = []
+        for i in range(len(self.dist)):
+            distTotal += int(i)
+            distList.append(int(i))
+
+        distSection = distTotal/distList[0]
+
+
+        chunks = [self.rawData[j:j+n] for j in range(int(self.bytes))]
+
+        
+
